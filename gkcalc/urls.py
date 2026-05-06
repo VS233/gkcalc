1e61c0ee2986e39c08521add7cf3b18bf526feb5
viktor shirkin <v230786@gmail.com>
2026-05-06 15:14:03 +0500

    seo

--- gkcalc/urls.py
+++ gkcalc/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from calculator.sitemaps import StaticSitemap
+
+sitemaps = {'static': StaticSitemap()}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +13,7 @@ urlpatterns = [
     path('', include('calculator.urls')),
     path('catalog/', include('catalog.urls')),
     path('skills/', include('skills.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
 if settings.DEBUG:
